2bae2513da8beff06f12be47be547781d531ebe7
YakovlevDanylo <yakovlevdv31@gmail.com>
2024-06-15 22:19:17 +0300

    Init project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
